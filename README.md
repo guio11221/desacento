73c8a8d5f756f7f2a0d9f8d9f4fdd230bbec1bd4
Guio11221 <sguii5147@gmail.com>
2023-09-02 20:19:44 -0400

    02/09/23

--- README.md
+++ README.md
@@ -1,1 +1,144 @@
-# desacento
+
+# Desacento# Função de Processamento de Texto
+
+Esta função permite realizar várias operações de processamento de texto em uma string. Ela oferece uma variedade de opções para personalizar o processamento de acordo com suas necessidades.
+
+## Como Usar
+
+Para usar a função, primeiro importe-a no seu código:
+
+```javascript
+const processText = require('./sua-funcao-de-processamento-de-texto');
+```
+
+### A seguir, você pode chamar a função processText() com a string que deseja processar e um objeto de opções.
+
+```
+const texto = "Texto de Exemplo";
+  const opcoes = {
+    // Opções de processamento aqui
+  };
+
+const resultado = processText(texto, opcoes);
+console.log(resultado);
+
+```
+
+# As opções disponíveis e como usá-las:
+
+## Remover Acentos :
+
+#### Use removeAccents: true para remover acentos da string e toLowerCase: true para converter para letras minúsculas:
+```
+ const opcoes = { removeAccents: true, toLowerCase: true };
+
+```
+
+## Remover Caracteres Especiais :
+
+#### Use removeSpecialChars: true para remover caracteres especiais da string:
+```
+ const opcoes = { removeSpecialChars: true };
+```
+
+## Substituir Espaços :
+
+#### Use replaceSpaces para substituir espaços em branco por um caractere específico, por exemplo, '-' :
+```
+const opcoes = { replaceSpaces: '-' };
+
+```
+
+## Adicionar Prefixo e Sufixo :
+
+#### Use addPrefix para adicionar um prefixo e addSuffix para adicionar um sufixo à string:
+
+```
+const opcoes = { addPrefix: 'Prefixo-', addSuffix: '-Sufixo' };
+
+```
+
+## Truncar Texto
+
+#### Use truncateText para truncar a string para um número específico de caracteres:
+
+```
+const opcoes = { truncateText: 10 };
+
+```
+
+## Validar Endereço de E-mail
+
+#### Use validateEmail: true para validar se a string é um endereço de e-mail válido:
+
+```
+const opcoes = { validateEmail: true };
+
+```
+
+## Validar URL
+
+#### Use validateUrl: true para validar se a string é uma URL válida:
+
+```
+const opcoes = { validateUrl: true };
+
+```
+
+## Contar Palavras
+
+#### Use countWords: true para contar o número de palavras na string:
+
+```
+const opcoes = { countWords: true };
+
+```
+
+## Substituir Texto
+
+#### Use replaceText para substituir uma sequência por outra em toda a string. Por exemplo, para substituir todos os 'a' por 'x':
+
+```
+const opcoes = { replaceText: { 'a': 'x' } };
+
+```
+
+## Normalização Unicode
+
+#### Use normalizeUnicode: true para normalizar caracteres Unicode:
+
+```
+const opcoes = { normalizeUnicode: true };
+
+```
+
+## Inverter Texto
+
+#### Use reverseText: true para inverter a ordem dos caracteres na string:
+```
+const opcoes = { reverseText: true };
+
+```
+
+## Separar em Palavras
+
+#### Use splitWords: true para dividir a string em um array de palavras:
+
+```
+const opcoes = { splitWords: true };
+
+```
+## Validar Data
+
+### Use validateDate: true para validar a data
+
+```
+const opcoes = { validateDate: true }
+```
+
+## Contribuições
+
+Sinta-se à vontade para contribuir com melhorias nesta função. Abra um problema ou envie uma solicitação pull com suas alterações.
+
+
+Este README fornece instruções claras sobre como usar cada opção da função de processamento de texto, tornando mais fácil para os usuários entenderem como personalizar o processamento de acordo com suas necessidades específicas. Certifique-se de adaptar as instruções de acordo com a implementação real da função.
--- index.js
+++ index.js
@@ -1,18 +1,170 @@
-module.exports = function (str) {
-  // Mapeia os caracteres acentuados para seus equivalentes não acentuados
-  var accentChars = 'àáâãçéêíóôõúüÀÁÂÃÇÉÊÍÓÔÕÚÜ';
-  var unaccentChars = 'aaaaceeiooouuAAAACEEIOOOUU';
+const { removeAccents, removeSpecialChars, toLowerCase, extractSubstring, isValidDate } = require("./func/myFunction")
 
-  // Verifica se o input é uma string
-  if (typeof str === 'string') {
-    // Substitui cada caractere acentuado pelo equivalente não acentuado
-    var unaccentedStr = str.replace(/./g, function (char) {
-      var index = accentChars.indexOf(char);
-      return index !== -1 ? unaccentChars[index] : char;
-    });
-
-    return unaccentedStr;
-  } else {
+module.exports = function (str, options) {
+  if (typeof str !== 'string') {
     return str; // Retorna o input sem modificação se não for uma string
   }
+
+  // Verifica se options é um objeto
+  options = typeof options === 'object' ? options : {};
+
+  // Aplica as opções selecionadas
+  if (options.removeAccents) {
+    str = removeAccents(str);
+  }
+
+  if (options.removeSpecialChars) {
+    str = removeSpecialChars(str);
+  }
+
+  if (options.toLowerCase) {
+    str = toLowerCase(str);
+  }
+
+  if (options.trimSpaces) {
+    str = str.trim();
+  }
+
+  if (options.replaceSpaces) {
+    str = str.replace(/\s/g, options.replaceSpaces);
+  }
+
+  if (options.maxLength && str.length > options.maxLength) {
+    str = str.slice(0, options.maxLength);
+  }
+
+  if (options.capitalizeFirstLetter) {
+    str = str.replace(/(?:^|\s)\S/g, function (a) {
+      return a.toUpperCase();
+    });
+  }
+
+  if (options.removeNumbers) {
+    str = str.replace(/[0-9]/g, '');
+  }
+
+  if (options.customReplace) {
+    for (var key in options.customReplace) {
+      if (options.customReplace.hasOwnProperty(key)) {
+        str = str.replace(new RegExp(key, 'g'), options.customReplace[key]);
+      }
+    }
+  }
+
+  // Remove espaços duplicados
+  if (options.removeDuplicateSpaces) {
+    str = str.replace(/\s+/g, ' ');
+  }
+
+  // Remove tags HTML
+  if (options.removeHtmlTags) {
+    str = str.replace(/<[^>]*>/g, '');
+  }
+
+  // Converte caracteres especiais HTML
+  if (options.convertHtmlSpecialChars) {
+    str = str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  }
+
+  // Substitui texto
+  if (options.replaceText) {
+    for (var key in options.replaceText) {
+      if (options.replaceText.hasOwnProperty(key)) {
+        str = str.replace(new RegExp(key, 'g'), options.replaceText[key]);
+      }
+    }
+  }
+
+  // Conta palavras
+  if (options.countWords) {
+    var words = str.split(/\s+/);
+    return words.length;
+  }
+
+  // Valida email
+  if (options.validateEmail) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(str);
+  }
+
+  // Valida URL
+  if (options.validateUrl) {
+    var urlPattern = /^(https?:\/\/)?([a-z0-9-]+\.)+[a-z]{2,6}([/?].*)?$/i;
+    return urlPattern.test(str);
+  }
+
+  // Truncar texto
+  if (options.truncateText) {
+    if (str.length > options.truncateText) {
+      return str.slice(0, options.truncateText) + '...';
+    }
+  }
+
+  // Normalização Unicode
+  if (options.normalizeUnicode) {
+    str = str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  // Ordenar palavras
+  if (options.sortWords) {
+    var words = str.split(/\s+/);
+    words.sort();
+    return words.join(' ');
+  }
+
+  // Inverter texto
+  if (options.reverseText) {
+    str = str.split('').reverse().join('');
+  }
+
+  // Remove caracteres específicos
+  if (options.removeSpecificChars) {
+    var charsToRemove = options.removeSpecificChars.split('');
+    for (var i = 0; i < charsToRemove.length; i++) {
+      var charToRemove = charsToRemove[i];
+      var regex = new RegExp(charToRemove, 'g');
+      str = str.replace(regex, '');
+    }
+  }
+
+  // Adicionar prefixo
+  if (options.addPrefix) {
+    str = options.addPrefix + str;
+  }
+
+  // Adicionar sufixo
+  if (options.addSuffix) {
+    str = str + options.addSuffix;
+  }
+
+  // Verificar se começa com
+  if (options.startsWith) {
+    if (!str.startsWith(options.startsWith)) {
+      return false;
+    }
+  }
+
+  // Verificar se termina com
+  if (options.endsWith) {
+    if (!str.endsWith(options.endsWith)) {
+      return false;
+    }
+  }
+
+  // Separar em palavras
+  if (options.splitWords) {
+    return str.split(/\s+/);
+  }
+
+  if (options.extractSubstring) {
+    var start = options.extractSubstring.start || 0;
+    var end = options.extractSubstring.end || str.length;
+    str = extractSubstring(str, start, end);
+  }
+
+  if(options.validateDate){
+    return isValidDate(str)
+  }
+
+  return str;
 };
